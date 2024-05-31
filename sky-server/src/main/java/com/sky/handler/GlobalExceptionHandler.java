4b6288e9cfe09a3a4b8c8a6b758a38d1dc7bbdb1
luoyutao <2218325920@qq.com>
2024-05-31 10:26:50 +0800

    苍穹外卖新增员工代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +26,18 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException exception) {
+        String msg=exception.getMessage();
+        if(msg.contains("Duplicate")) {
+            String[] split = msg.split(" ");
+            String username=split[2];
+            String errMsg=split[2]+ MessageConstant.ALREADY_EXISTS;
+            return Result.error(errMsg);
+        } else {
+            return Result.error(MessageConstant.ALREADY_EXISTS);
+        }
     }
 
 }
